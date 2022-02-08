f92be2026afc06f43091a2ab6b6cb7965bb43a1d
Philippe Beck <philippe@philippebeck.net>
2022-02-08 22:03:25 +0100

    Update app for MVC

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,17 @@
 "use strict";
 
-const express = require("express");
-const app     = express();
+const express     = require("express");
+const app         = express();
+const mongoose    = require("mongoose");
+const thingRoute  = require('./routes/ThingRoute');
+
+require("dotenv").config();
+
+mongoose.connect(process.env.DB_CONNECT,
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
 
 app.use(express.json());
 
@@ -21,34 +31,6 @@ app.use((req, res, next) => {
   next();
 });
 
-app.post("/api/stuff", (req, res, next) => {
-  console.log(req.body);
-
-  res.status(201).json({
-    message: "Objet créé !"
-  });
-});
-
-app.get("/api/stuff", (req, res, next) => {
-  const stuff = [
-    {
-      _id: "oeihfzeoi",
-      title: "Mon premier objet",
-      description: "Les infos de mon premier objet",
-      imageUrl: "https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg",
-      price: 4900,
-      userId: "qsomihvqios",
-    },
-    {
-      _id: "oeihfzeomoihi",
-      title: "Mon deuxième objet",
-      description: "Les infos de mon deuxième objet",
-      imageUrl: "https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg",
-      price: 2900,
-      userId: "qsomihvqios",
-    },
-  ];
-  res.status(200).json(stuff);
-});
+app.use('/api/stuff', thingRoute);
 
 module.exports = app;

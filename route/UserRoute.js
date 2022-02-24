286124bf141261e3066ad1573f3d0dc4c67d4958
Philippe Beck <philippe@philippebeck.net>
2022-02-24 22:26:07 +0100

    Update UserRoute for middleware

--- route/UserRoute.js
+++ route/UserRoute.js
@@ -3,14 +3,17 @@
 const express = require("express");
 const router  = express.Router();
 
+const jwt     = require("../middleware/jwt");
+const multer  = require("../middleware/multer");
+
 const UserCtrl = require("../controller/UserCtrl");
 
 router.post("/signup", UserCtrl.signup);
 router.post("/login", UserCtrl.login);
-router.get("/list", UserCtrl.list);
-router.post("/create", UserCtrl.create);
-router.get("/read:id", UserCtrl.read);
-router.put("/update:id", UserCtrl.update);
-router.delete("/delete:id", UserCtrl.delete);
+router.get("/list", jwt, UserCtrl.list);
+router.post("/create", jwt, multer, UserCtrl.create);
+router.get("/read:id", jwt, UserCtrl.read);
+router.put("/update:id", jwt, multer, UserCtrl.update);
+router.delete("/delete:id", jwt, UserCtrl.delete);
 
 module.exports = router;

781bb53d36d595e90e646c6f9af536739e62eb34
Philippe Beck <philippe@philippebeck.net>
2022-02-24 22:10:34 +0100

    Update UserRoute

--- route/UserRoute.js
+++ route/UserRoute.js
@@ -7,5 +7,10 @@ const UserCtrl = require("../controller/UserCtrl");
 
 router.post("/signup", UserCtrl.signup);
 router.post("/login", UserCtrl.login);
+router.get("/list", UserCtrl.list);
+router.post("/create", UserCtrl.create);
+router.get("/read:id", UserCtrl.read);
+router.put("/update:id", UserCtrl.update);
+router.delete("/delete:id", UserCtrl.delete);
 
 module.exports = router;
